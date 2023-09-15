ed5f292b0364443ce334c79b81e016f891b50fc6
CT Shen <32235978+ChunTingShen@users.noreply.github.com>
2023-09-15 18:40:56 +0800

    create service

--- blogpost/src/app/app.component.ts
+++ blogpost/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogService } from './blog.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'blogpost';
+export class AppComponent implements OnInit{
+  constructor(private blogService: BlogService){}
+  
+  title = "hihihi";
+
+  ngOnInit(): void {
+      this.getData();
+  }
+
+  getData() {
+    console.log(
+      "In Get Data"
+    )
+    this.blogService.getPost().subscribe((res)=>{
+      console.log(res)
+    })
+  }
+
 }
---
+++ blogpost/src/app/blog.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogService {
+  constructor(private httpClient: HttpClient) { }
+
+  private readonly url = "https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json";
+
+
+  getPost(){
+    console.log(this.url)
+    return this.httpClient.get(this.url);
+  }
+}
+
+export interface Post {
+  id: number;
+  date: string;
+  date_gmt: string;
+  guid: Rendered;
+  modified: string;
+  modified_gmt: string;
+  slug: string;
+  status: string;
+  type: string;
+  link: string;
+  title: Rendered;
+  content: Rendered;
+  excerpt: Rendered;
+  author: number;
+  comment_status: string;
+  ping_status: string;
+  sticky: string;
+  template: string;
+  format: string;
+  meta: string[];
+  categories: number[];
+  tags: number[];
+  topic: number[];
+  group: number[];
+  _event_location: string;
+  _event_venue: string;
+  _event_registration: string;
+  _start_day: string;
+  _start_month: string;
+  _start_year: string;
+  _end_day: string;
+  _end_month: string;
+  _end_year: string;
+  about: About;
+  // author: Author;
+
+}
+
+export interface Rendered {
+  rendered: string
+}
+
+export interface About{
+  href: string;
+
+}
+
+export interface Author {
+  embeddable: string;
+  href: string
+}
